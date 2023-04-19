e8d98208e996f1c586ac1667f59d29d6efde2154
Sandvoxel <kovalchickzz@gmail.com>
2023-04-18 21:33:26 -0500

    Update crash on fresh install

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 ## This is best used in WSL 
 Required to Run in WSL
 ```shell
-sudo apt install vlc vlclib-dev
+sudo apt install mpv libmpv-dev
 ```
+
+http://198.28.182.104:8111/guestAuth/app/rest/builds/buildType:ClueDevice_Build,status:SUCCESS/artifacts/content/clue-device
+
--- src/config/setup.rs
+++ src/config/setup.rs
@@ -1,6 +1,8 @@
+use std::fs;
 use std::fs::{File, OpenOptions};
 use std::io::{Read, Write};
 use std::path::{PathBuf};
+use log::error;
 
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
@@ -28,12 +30,8 @@ impl DeviceConfiguration {
         if !path.is_file() {
             // If the YAML file doesn't exist, create it and save the struct as YAML
             device_config = DeviceConfiguration::new();
+            device_config.save(path);
 
-            let serialized_yaml = serde_yaml::to_string(&device_config).unwrap();
-            let mut file = File::create(path.clone()).expect("Unable to create file");
-            file.write_all(serialized_yaml.as_bytes())
-                .expect("Unable to write data to file");
-            println!("Config file created: {}", path.display());
         } else {
             // If the YAML file exists, read and parse it into the struct
             let mut file = OpenOptions::new()
@@ -42,10 +40,27 @@ impl DeviceConfiguration {
                 .expect("Unable to open config");
             let mut contents = String::new();
             file.read_to_string(&mut contents).expect("Unable to read file");
-            device_config = serde_yaml::from_str(&contents).expect("Failed to parse config file");
+            device_config = serde_yaml::from_str(& contents).expect("Failed to parse config file");
             println!("Config file read: {:?}", device_config);
         }
 
         device_config
+    }
+
+    pub fn save(&self, path: PathBuf){
+        let mut parent_dir = path.clone();
+        parent_dir.pop();
+        if !parent_dir.is_dir(){
+            fs::create_dir_all(parent_dir).unwrap_or_else(|err|{
+                error!("Faired to create database dir: {:?}", err);
+                panic!()
+            })
+        }
+
+        let serialized_yaml = serde_yaml::to_string(self).unwrap();
+        let mut file = File::create(path.clone()).expect("Unable to create file");
+        file.write_all(serialized_yaml.as_bytes())
+            .expect("Unable to write data to file");
+        println!("Config file created: {}", path.display());
     }
 }

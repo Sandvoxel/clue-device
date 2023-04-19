baabea5574023472ea4cefeb7ba163ded7b14bf7
Sandvoxel <kovalchickzz@gmail.com>
2023-04-18 23:59:29 -0500

    Test change

--- src/logging/logging_util.rs
+++ src/logging/logging_util.rs
@@ -9,8 +9,6 @@ use log4rs::config::RawConfig;
 use crate::config::setup::DeviceConfiguration;
 
 pub fn setup_logging(device_config: &DeviceConfiguration) -> Result<(),  Box<dyn std::error::Error>> {
-
-
     let device_id = device_config.device_uuid.clone();
     let binding = include_str!("../../config/log4rs.yaml").replace("{device_id}", device_id.as_str());
     let config_str = binding.as_str();

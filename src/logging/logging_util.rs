f13ef651856318ae085a03044e14a79eec88b765
Sandvoxel <kovalchickzz@gmail.com>
2023-04-19 20:46:11 -0500

    Added remote logging

--- src/logging/logging_util.rs
+++ src/logging/logging_util.rs
@@ -1,5 +1,8 @@
+use std::collections::HashMap;
 use log4rs::Config;
 use log4rs::config::{InitError, RawConfig};
+use qoollo_log4rs_logstash::config::DeserializersExt;
+use serde_json::Value;
 
 use crate::config::setup::DeviceConfiguration;
 
@@ -9,9 +12,13 @@ pub fn setup_logging(device_config: &DeviceConfiguration) -> Result<(),  Box<dyn
     let config_str = binding.as_str();
     let config: RawConfig = serde_yaml::from_str(config_str).unwrap();
 
-    let (appenders, errors) = config.appenders_lossy(log4rs::config::Deserializers::default().with_logstash());
+    let mut data:  HashMap<String, Value>  = HashMap::new();
+
+    data.insert("device-id".to_string(), Value::String(device_config.device_uuid.clone()));
+
+    let (appenders, errors) = config.appenders_lossy(&log4rs::config::Deserializers::default().with_logstash_extra(data));
     if !errors.is_empty() {
-        return panic!();
+        panic!();
     }
     let config = Config::builder()
         .appenders(appenders)
